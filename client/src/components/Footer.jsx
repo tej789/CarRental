b3d37ca6980e46f20c514c821e51b78ac933317b
tej789 <gotitej2005@gmail.com>
2026-03-21 17:02:47 +0530

    Edit Contact Details

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -70,10 +70,10 @@ const Footer = () => {
                 <div>
                     <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
                     <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li>1234 Luxury Drive</li>
-                        <li>San Francisco, CA 94107</li>
-                        <li>+1 234 567890</li>
-                        <li>info@example.com</li>
+                        <li>Nana Varachha</li>
+                        <li>Surat, Gujarat</li>
+                        <li>+918160515463</li>
+                        <li>gotitej2005@gmail.com</li>
                     </ul>
                 </div>
 
